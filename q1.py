599a1782ee1870de96c366a981e6e5972a3f4cf8
jonaathan22 <jonaath.k@gmail.com>
2025-10-15 03:41:21 +0530

    changes

---
+++ q1.py
@@ -0,0 +1,10 @@
+number = int(input())
+if number>0:
+   if number%2 == 0:
+    print("postive and even")
+   else:
+    print("postive and odd")
+if number<0:
+   print("negative")
+else:
+ print ("zero")
